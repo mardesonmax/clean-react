108996bb4e33aa6b8f7b049ba3f1c8943b6e3eac
Mardeson Pereira <maxpb777@gmail.com>
2022-07-21 16:48:02 -0300

    test: ensure RemoteAuthentication throws UnexpectedError

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -2,6 +2,7 @@ import { faker } from '@faker-js/faker';
 import { HttpStatusCode } from '~/data/protocols/http/http-reponse';
 import { HttpPostClientSpy } from '~/data/test/mock-http-client';
 import { InvalidCredentialsError } from '~/domain/errors/invalid-credentials-error';
+import { UnexpectedError } from '~/domain/errors/unexpected-error';
 import { mockAuthentication } from '~/domain/test/mock-authentication';
 import { RemoteAuthentication } from './remote-authentication';
 
@@ -51,5 +52,41 @@ describe('RemoteAuthentication', () => {
     const promise = sut.auth(mockAuthentication());
 
     expect(promise).rejects.toThrow(new InvalidCredentialsError());
+  });
+
+  test('should throw InvalidCredentialsError if HttpPostClient returns 400', async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest,
+    };
+
+    const promise = sut.auth(mockAuthentication());
+
+    expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+
+  test('should throw InvalidCredentialsError if HttpPostClient returns 500', async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.serverError,
+    };
+
+    const promise = sut.auth(mockAuthentication());
+
+    expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+
+  test('should throw InvalidCredentialsError if HttpPostClient returns 404', async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.notFound,
+    };
+
+    const promise = sut.auth(mockAuthentication());
+
+    expect(promise).rejects.toThrow(new UnexpectedError());
   });
 });
