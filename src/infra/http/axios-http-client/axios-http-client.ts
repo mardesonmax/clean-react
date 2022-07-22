d6c5724650251a796af1bfd3ab58b272741c8d36
Mardeson Pereira <maxpb777@gmail.com>
2022-07-22 08:56:15 -0300

    feat: ensure AxiosHttpCleint calls axios with correct URL

---
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import { HttpPostPrams } from '~/data/protocols/http';
+
+export class AxiosHttpClient {
+  // eslint-disable-next-line class-methods-use-this
+  async post(params: HttpPostPrams<any>): Promise<void> {
+    await axios(params.url);
+  }
+}
