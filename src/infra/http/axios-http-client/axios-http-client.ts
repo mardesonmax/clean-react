8891901fd99d5ccd9ffff05150fd585ee9738d64
Mardeson Pereira <maxpb777@gmail.com>
2022-07-22 09:03:06 -0300

    feat: ensure AxiosHttpCleint calls axios with correct verb

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -4,6 +4,6 @@ import { HttpPostPrams } from '~/data/protocols/http';
 export class AxiosHttpClient {
   // eslint-disable-next-line class-methods-use-this
   async post(params: HttpPostPrams<any>): Promise<void> {
-    await axios(params.url);
+    await axios.post(params.url);
   }
 }
